3834fdc47d2414ef0c02c94be6fdec5fe7a8b798
unknown <studzinskis46@gmail.com>
2023-11-13 17:17:12 +0100

    feat: Backend prac- git commands

---
+++ backend-exercises/githubCommands.sh
@@ -0,0 +1,45 @@
+# Initialize a new Git repository
+git init
+
+# Clone a remote repository
+git clone <repository-url>
+
+# Check the status of changes
+git status
+
+# Add changes to the staging area
+git add <file>
+
+# Commit changes with a message
+git commit -m "Commit message"
+
+# View commit history
+git log
+
+# Create a new branch
+git branch <branch-name>
+
+# Switch to an existing branch
+git checkout <branch-name>
+
+# Create and switch to a new branch
+git checkout -b <new-branch-name>
+
+# Merge changes from another branch
+git merge <branch-name>
+
+# Fetch changes from a remote repository
+git fetch
+
+# Pull changes from a remote repository
+git pull
+
+# Push changes to a remote repository
+git push origin <branch-name>
+
+# Configure user information
+git config --global user.name "Your Name"
+git config --global user.email "your@email.com"
+
+# Ignore files by creating a .gitignore file
+# Add filenames or patterns to be ignored
