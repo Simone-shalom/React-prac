3ba74673cca80ef34d210549211fe89ab930e27f
unknown <studzinskis46@gmail.com>
2023-11-15 14:04:49 +0100

    feat: Js prac - scopesInVarConst

---
+++ js-coding-exercises/scopesInVarConst.js
@@ -0,0 +1,22 @@
+//var logs count 
+function x (){
+
+    if(true){
+      var count = 'count'
+    }
+    console.log(count)
+  }
+  
+  x()
+  
+
+  //const count is not defined
+function x (){
+
+    if(true){
+      const count = 'count'
+    }
+    console.log(count)
+  }
+  
+  x()
