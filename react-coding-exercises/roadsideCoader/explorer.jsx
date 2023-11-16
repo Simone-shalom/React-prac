558f96a09737c1f850132274517d92dc764a1b0f
unknown <studzinskis46@gmail.com>
2023-11-16 12:44:55 +0100

    feat: React prac - fileExplorer + files handling

--- react-coding-exercises/roadsideCoader/explorer.jsx
+++ react-coding-exercises/roadsideCoader/explorer.jsx
@@ -5,57 +5,69 @@ import React, { useState } from "react";
 import "./tailwind.output.css";
 
 const App = () => {
-
   return (
     <div className="min-h-screen ">
       <h2 className="text-center text-4xl font-bold">Explorer data</h2>
-      <Folder data={data}/>
+      <Folder data={data} />
     </div>
   );
 };
 
 export default App;
 
-
-export const Folder = ({data}) => {
-
-  const [open, setOpen] = useState(false)
-
+export const Folder = ({ data }) => {
+  const [open, setOpen] = useState(false);
+  const [file, setFile] = useState(null);
 
   const handleOpen = () => {
-    setOpen(!open)
-  }
+    setOpen(!open);
+  };
 
-  if(data.isFolder){
+  const handleFile = (data) => {
+    // Toggle the selected file
+    setFile((prevFile) => (prevFile === data ? null : data));
+  };
+  console.log(file);
 
+  if (data.isFolder) {
     return (
-      <div className='flex flex-col items-start px-5'>
-        <button  
-          onClick={handleOpen} 
+      <div className="flex flex-col items-start px-5">
+        <button
+          onClick={handleOpen}
           className={`cursor-pointer focus:outline-none
-           ${open ? 'bg-gray-200' : ''}`}>
+           ${open ? "bg-gray-200" : ""}`}
+        >
           {data.name}
         </button>
         {open ? (
           <div>
             {data.items.map((item) => (
-              <Folder data={item} key={item.name}/>
+              <Folder data={item} key={item.name} />
             ))}
           </div>
-        ) : ''}
-        </div>
-    )
-
+        ) : (
+          ""
+        )}
+      </div>
+    );
   } else {
     return (
-      <div className='flex flex-col px-5'>
+      <div
+        className={`flex flex-col px-5`}
+        onClick={() => handleFile(data.name)}
+      >
         {data.name}
-        </div>
-    )
+        {file === data.name && (
+          <p className="flex items-center justify-center">
+            Choosen file : {data.name}
+          </p>
+        )}
+      </div>
+    );
   }
-}
+};
 
- const data = {
+const data = {
   name: "src",
   isFolder: true,
   items: [
