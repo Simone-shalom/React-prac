985a258eb8464d930dfeafaa3528adf18fb6b2a8
unknown <studzinskis46@gmail.com>
2023-11-17 11:07:09 +0100

    feat: Js prac - isPangram -my answer

--- js-coding-exercises/isPangram.js
+++ js-coding-exercises/isPangram.js
@@ -22,4 +22,28 @@ function isPangram(sentence) {
   // Example usage
   const exampleSentence = "The quick brown fox jumps over the lazy dog";
   console.log(isPangram(exampleSentence)); // Output: true
+
+
+  // My answer  
+function isPangram(sentence){
+
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const lowercaseSentence = sentence.toLowerCase()
   
+    let sizeOfAlphabet = 0
+  
+    const uniqueSentence = new Set(lowercaseSentence)
+  
+    for(let l of uniqueSentence){
+       if(alphabet.includes(l)){
+         sizeOfAlphabet ++
+       }
+      }
+      if(sizeOfAlphabet === 26){
+        return true
+      } else {
+        return false
+      }
+    } 
+  
+  console.log(isPangram(" quick brown fox jumps over lazy dog"))
