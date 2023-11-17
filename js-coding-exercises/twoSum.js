6b0cc3d330117e56a40f63c3378f437cd0bcd7e9
unknown <studzinskis46@gmail.com>
2023-11-17 17:51:48 +0100

    feat: Js prac - twoSum

---
+++ js-coding-exercises/twoSum.js
@@ -0,0 +1,15 @@
+
+// two sum shit
+
+function twoSum(numbers, target){
+  
+    for(let i = 0; i<numbers.length; i++){
+      for(let j = i+1 ; j < numbers.length; j++){
+        if(numbers[i] + numbers[j] === target){
+          return [i, j]
+        }
+      }
+    }
+    return {numbers, target}
+  }
+  console.log(twoSum([1,23,4,5], 9))
