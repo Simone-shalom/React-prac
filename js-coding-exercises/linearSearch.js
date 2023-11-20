857a3dbfebe6ac6e72c893bd96a7545d82497f97
unknown <studzinskis46@gmail.com>
2023-11-20 13:41:36 +0100

    feat: Js prac- globalSearch

--- js-coding-exercises/linearSearch.js
+++ js-coding-exercises/linearSearch.js
@@ -12,3 +12,26 @@ function search(array, target){
    }
    
    console.log(search([1,5,2,6,8],2))
+
+
+   
+//global Search
+
+
+function searchG(array, target){
+
+    const result = []
+  
+    for(let i = 0; i<= array.length; i++){
+      if(array[i] === target){
+        result.push(i)
+      }
+    }
+    if(result ===0 ){
+      return -1
+    } else {
+      return result
+    }
+   }
+   
+   console.log(searchG([1,5,2,6,8,6,3,6],6))
