92e2389cb20ea6912be50684c9ffc820314997bc
unknown <studzinskis46@gmail.com>
2023-11-20 12:07:17 +0100

    feat: Js prac- hamming distance of 2 bits

--- js-coding-exercises/hammingDistance.js
+++ js-coding-exercises/hammingDistance.js
@@ -46,3 +46,31 @@ function hammingDistance2(x, y){
   
   
   console.log(hammingDistance2('subscribe', 'sutecribe'))
+
+  
+// hamming distance of Bits of 2 ints 
+function hammingDistance2(x, y) {
+    let distance = 0;
+  
+    x = x.toString(2);
+    y = y.toString(2);
+  
+    // Make the lengths equal by adding leading zeros
+    while (x.length < y.length) {
+      x = '0' + x;
+    }
+  
+    while (y.length < x.length) {
+      y = '0' + y;
+    }
+  
+    for (let i = 0; i < x.length; i++) {
+      if (x[i] !== y[i]) {
+        distance++;
+      }
+    }
+  
+    return distance;
+  }
+  
+  console.log(hammingDistance2(1, 11));
