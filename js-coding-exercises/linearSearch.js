37e90da55094c184720ef7be3f413b5216beb0b5
unknown <studzinskis46@gmail.com>
2023-11-20 13:36:14 +0100

    feat: Js prac- LinearSearch

---
+++ js-coding-exercises/linearSearch.js
@@ -0,0 +1,14 @@
+
+
+// Linear search 
+function search(array, target){
+
+    for(let i = 0; i<= array.length; i++){
+      if(array[i] === target){
+        return i
+      }
+    }
+    return -1
+   }
+   
+   console.log(search([1,5,2,6,8],2))
