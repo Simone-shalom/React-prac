0039f833cc637e30782a2ccbbc468814db6913cd
unknown <studzinskis46@gmail.com>
2023-12-07 21:16:34 +0100

    feat: React prac -Timer with pause - interval

--- react-coding-exercises/dailyPrac/two.tsx
+++ react-coding-exercises/dailyPrac/two.tsx
@@ -59,3 +59,45 @@ interface ItemProps {
 export const Item = ({ item }:ItemProps) => {
   return <div className="flex items-center flex-col">{item}</div>;
 };
+
+
+
+//wokring timer with pause - 
+// Dependencies
+import React, { useEffect, useState } from "react";
+
+// Styles
+import "./tailwind.output.css";
+
+const App = () => {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [running, setRunning] = useState(true);
+
+  useEffect(() => {
+    let interval;
+
+    if (running) {
+      interval = setInterval(() => {
+        setTime(new Date().toLocaleTimeString());
+      }, 1000);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [running]);
+
+  const onPause = () => {
+    setRunning(!running);
+  };
+
+  return (
+    <div className="min-h-screen px-10 flex items-center flex-col">
+      <h1 className="text-2xl font-semibold">Timer comp</h1>
+
+      <div>{time}</div>
+      <button onClick={onPause}>Stop</button>
+    </div>
+  );
+};
+
+export default App;
