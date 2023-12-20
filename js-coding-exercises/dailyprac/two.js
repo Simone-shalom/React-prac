1b7e782ff5ea571f3d3b0625641479b65b418a95
unknown <studzinskis46@gmail.com>
2023-12-20 22:03:48 +0100

    feat:Js prac- hoisting

--- js-coding-exercises/dailyprac/two.js
+++ js-coding-exercises/dailyprac/two.js
@@ -45,3 +45,28 @@ Promise.allSettled([displayData(), displayData2(), displayData3()])
 Promise.race([displayData(), displayData2(), displayData3()]).then((result) =>
   console.log(result)
 );
+
+//1
+console.log(foo);
+foo = 1;
+//foo is not defined
+
+//2
+console.log(foo);
+var foo = 1;
+//undefined
+
+//3
+foo = 3;
+console.log(foo);
+var foo = 1;
+//just 3
+
+//function hoisting works everytime
+foo();
+function foo() {
+  console.log("Hosting");
+}
+
+//let and const are hoisting in temporary dead zone,
+// wihtout decl
